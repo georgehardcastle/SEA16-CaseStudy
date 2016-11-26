eab2b358d6428b7aa351466d85943d2e8af1771d
Jackson, Ceri-anne (Associate Software Developer) <ceri-anne.jackson@sky.uk>
2016-11-26 15:47:40 +0000

    Added additional details to Signup page

--- SEA16-CaseStudy/config/passport.js
+++ SEA16-CaseStudy/config/passport.js
@@ -13,12 +13,23 @@ passport.deserializeUser(function(id, done) {
 });
 
 passport.use('local.signup', new LocalStrategy({
+
   usernameField: 'email',
   passwordField: 'password',
   passReqToCallback: true
+
 }, function(req, email, password, done){
+
   req.checkBody('email', 'Invalid email').notEmpty().isEmail();
   req.checkBody('password', 'Invalid password').notEmpty().isLength({min:4});
+  req.checkBody('firstname', 'Invalid firstname').notEmpty().isLength({min:2});
+  req.checkBody('lastname', 'Invalid lastname').notEmpty().isLength({min:2});
+  req.checkBody('firstlineofaddress', 'Invalid first line of address').notEmpty().isLength({min:4});
+  req.checkBody('town', 'Invalid town').notEmpty().isLength({min:2});
+  req.checkBody('postcode', 'Invalid postcode').notEmpty().isLength({min:4});
+  req.checkBody('contactnumber', 'Invalid contact number').notEmpty().isLength({min:4});
+
+
   var errors = req.validationErrors();
   if (errors) {
     var messages = [];
@@ -37,6 +48,13 @@ passport.use('local.signup', new LocalStrategy({
     var newUser = new User();
     newUser.email = email;
     newUser.password = newUser.encryptPassword(password);
+    newUser.firstname = req.body.firstname;
+    newUser.lastname = req.body.lastname;
+    newUser.firstlineofaddress = req.body.firstlineofaddress;
+    newUser.town = req.body.town;
+    newUser.postcode = req.body.postcode;
+    newUser.contactnumber = req.body.contactnumber;
+
     newUser.save(function(err, result) {
       if (err) {
         return done(err);
